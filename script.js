7fd22f0f7f921bfc54551c9b4ee2afcf0856d540
Vinny Murphy <vinnywebdev@gmail.com>
2024-01-02 08:40:08 +0000

    added buttons for input

--- script.js
+++ script.js
@@ -1,73 +1,67 @@
+//DOM
+/*
+we create a nodelist with all the buttons of a certain id.
+The forEach loop assigns each one the same functionality
+*/
+const buttons = document.querySelectorAll("#btn");
+
+//see callback function
+buttons.forEach((btn) => {
+  /*I've inserted an arrow function which calls playRound. 
+   if I try to call playround itslef, i either can't pass the values or it runs automatically.
+   For this reason it must be wrapped up in a callback function*/
+  btn.addEventListener("click", () => {
+    playRound(btn.value);
+  });
+});
+
+//btn.addEventListener("click", playRound);
 
 /*Function which radomly selects computer's choice of weapon*/
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3);
-    let computerChoice;
+  let num = Math.floor(Math.random() * 3);
+  let computerChoice;
 
-    if (num == 0) {
-        computerChoice = "Rock";
-    }
-    else if (num == 1) {
-        computerChoice = "Paper";
-    }
-    else {
-        computerChoice = "Scissors";
-    }
+  if (num == 0) {
+    computerChoice = "Rock";
+  } else if (num == 1) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
 
-    return computerChoice;
-
+  return computerChoice;
 }
 
-/*Function witch compares players choices and determines whether player wins or loses*/
-function playRound() {
-    let playerSelection = prompt("Rock, Paper or Scissors?");
-    let computerSelection = getComputerChoice();
-    /*converts all input to lowercase*/
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    /*If player chooses rock*/
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        alert("You win! Rock breaks scissors.");
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        alert( "You lose! Paper covers rock.");
+/*Function which compares players choices and determines whether player wins or loses*/
+function playRound(value) {
+  console.log("test");
 
-    }
+  let playerSelection = value;
+  console.log(playerSelection);
+  let computerSelection = getComputerChoice();
+  /*converts all input to lowercase*/
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  /*If player chooses rock*/
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You win! Rock breaks scissors.");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    alert("You lose! Paper covers rock.");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     /*If player chooses paper*/
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        alert("You win! Paper covers rock.");
-
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        alert("You lose! Scissors cuts paper.");
-
-    }
+    alert("You win! Paper covers rock.");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    alert("You lose! Scissors cuts paper.");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     /*If player chooses scissors*/
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        alert("You win! Scissors cuts paper.");
-
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        alert ("You lose! Rock breaks scissors.");
-
-    }
+    alert("You win! Scissors cuts paper.");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    alert("You lose! Rock breaks scissors.");
+  } else if (playerSelection === computerSelection) {
     /*If player and computer choose the same weapon*/
-    else if (playerSelection === computerSelection) {
-        alert("It's a tie! You've both selected the same.");
-
-    }
-    else {
-        alert("Invalid input");
-    }
-
+    alert("It's a tie! You've both selected the same.");
+  } else {
+    alert("Invalid input");
+  }
 }
-//loop to play the game three games
-function repeatGame() {
-    for (i = 0; i < 3; i++){
-        //computer generates new choice each time
-        getComputerChoice();
-        //play round of rock paper scissors
-        playRound();    
-    }
-}
-
